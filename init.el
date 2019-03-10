4323d90e72278a25819ee0e53815fece05d7524e
hawnzug <hawnzug@gmail.com>
2019-03-10 11:27:27 +0800

    org-super-agenda

--- init.el
+++ init.el
@@ -334,6 +334,22 @@
   (let ((date (org-date-to-gregorian (org-read-date))))
    (org-datetree-find-date-create date 'subtree-at-point)))
 
+(use-package org-super-agenda
+  :ensure t
+  :after org
+  :config
+  (org-super-agenda-mode)
+  (setq org-super-agenda-groups
+        '((:name "Clocking"
+                 :log t
+                 :and (:scheduled
+                       today
+                       :pred
+                       (lambda (item)
+                         (org-find-text-property-in-string 'time-of-day item))))
+          (:name "Others"
+                 :anything t))))
+
 (use-package geiser :ensure t
   :config
   (setq geiser-chez-binary "chez-scheme")
@@ -628,7 +644,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (expand-region proof-general highlight-indent-guides ivy-hydra general auctex lispyville all-the-icons-dired eshell-z esh-autosuggest elfeed-org elfeed org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil calfw-org calfw magit haskell-mode eyebrowse company-coq company use-package))))
+    (org-super-agenda expand-region proof-general highlight-indent-guides ivy-hydra general auctex lispyville all-the-icons-dired eshell-z esh-autosuggest elfeed-org elfeed org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil calfw-org calfw magit haskell-mode eyebrowse company-coq company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

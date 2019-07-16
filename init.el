60d71505c315349ef4ba40cbef0e16408374d2fe
hawnzug <hawnzug@gmail.com>
2019-07-16 19:39:03 +0800

    [hydra] Prettify

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  '(package-selected-packages
    '(emms caml-mode tuareg sublimity-scroll sublimity web-mode emmet-mode keyfreq org-tree-slide academic-phrases org-make-toc exec-path-from-shell xterm-color ivy-posframe avy fcitx moody ibuffer-vc dired-collapse dired-open dired-narrow symbol-overlay htmlize evil-matchit alert org-super-agenda proof-general ivy-hydra general auctex all-the-icons-dired eshell-z esh-autosuggest org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil magit eyebrowse company-coq company use-package))
  '(safe-local-variable-values
-   '((projectile-project-run-cmd . "make push")
+   '((org-babel-use-quick-and-dirty-noweb-expansion . t)
+     (projectile-project-run-cmd . "make push")
      (projectile-project-run-cmd . "make run")
      (projectile-project-compilation-cmd . "make"))))
 (custom-set-faces

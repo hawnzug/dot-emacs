c2bdf023532bf0b6f8ad43483c644ed32dc48b0c
hawnzug <hawnzug@gmail.com>
2019-09-19 23:27:56 -0500

    [anylink] Add notmuch-show-mode

--- packages/anylink/anylink.el
+++ packages/anylink/anylink.el
@@ -2,4 +2,5 @@
   (interactive)
   (cond
    ((eq major-mode 'org-mode) (org-open-at-point))
-   ((eq major-mode 'org-agenda-mode) (org-agenda-open-link))))
+   ((eq major-mode 'org-agenda-mode) (org-agenda-open-link))
+   ((eq major-mode 'notmuch-show-mode) (goto-address-at-point))))

6ddba302bf0b8632ad9a3feb4d02911c96b73b0c
hawnzug <hawnzug@gmail.com>
2019-09-23 22:34:18 -0500

    [org-gcal] Init

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(fancy-battery doom-themes doom-modeline yaml-mode sml-mode benchmark-init org-plus-contrib imenu-list emms tuareg sublimity-scroll sublimity web-mode emmet-mode keyfreq org-tree-slide academic-phrases org-make-toc exec-path-from-shell xterm-color ivy-posframe avy fcitx moody ibuffer-vc dired-collapse dired-open dired-narrow symbol-overlay htmlize evil-matchit alert org-super-agenda proof-general ivy-hydra general auctex all-the-icons-dired eshell-z esh-autosuggest org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil magit eyebrowse company-coq company use-package))
+   '(org-gcal fancy-battery doom-themes doom-modeline yaml-mode sml-mode benchmark-init org-plus-contrib imenu-list emms tuareg sublimity-scroll sublimity web-mode emmet-mode keyfreq org-tree-slide academic-phrases org-make-toc exec-path-from-shell xterm-color ivy-posframe avy fcitx moody ibuffer-vc dired-collapse dired-open dired-narrow symbol-overlay htmlize evil-matchit alert org-super-agenda proof-general ivy-hydra general auctex all-the-icons-dired eshell-z esh-autosuggest org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil magit eyebrowse company-coq company use-package))
  '(safe-local-variable-values
    '((org-babel-use-quick-and-dirty-noweb-expansion . t)
      (projectile-project-run-cmd . "make push")

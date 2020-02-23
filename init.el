5cd8544849553b24fa6f19fdbfaac5c0105d6efd
Zhuyang Wang <wang9163@umn.edu>
2020-02-22 18:43:51 -0600

    [flycheck] Use posframe instead of inline

--- init.el
+++ init.el
@@ -6,9 +6,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(find-file-in-project all-the-icons-ivy-rich ivy-rich olivetti ivy-bibtex terminal-here slime company-posframe flycheck-inline flycheck-rust cargo rust-mode docker-tramp dockerfile-mode csv-mode fancy-battery doom-themes doom-modeline yaml-mode sml-mode benchmark-init org-plus-contrib imenu-list emms tuareg sublimity-scroll sublimity web-mode emmet-mode keyfreq org-tree-slide academic-phrases org-make-toc exec-path-from-shell xterm-color ivy-posframe avy fcitx moody ibuffer-vc dired-collapse dired-open dired-narrow symbol-overlay htmlize evil-matchit alert proof-general ivy-hydra general auctex all-the-icons-dired eshell-z esh-autosuggest org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil magit eyebrowse company-coq company use-package))
- '(safe-local-variable-values
-   '((org-babel-use-quick-and-dirty-noweb-expansion . t))))
+   '(flycheck-posframe find-file-in-project all-the-icons-ivy-rich ivy-rich olivetti ivy-bibtex terminal-here slime company-posframe flycheck-inline flycheck-rust cargo rust-mode docker-tramp dockerfile-mode csv-mode fancy-battery doom-themes doom-modeline yaml-mode sml-mode benchmark-init org-plus-contrib imenu-list emms tuareg sublimity-scroll sublimity web-mode emmet-mode keyfreq org-tree-slide academic-phrases org-make-toc exec-path-from-shell xterm-color ivy-posframe avy fcitx moody ibuffer-vc dired-collapse dired-open dired-narrow symbol-overlay htmlize evil-matchit alert proof-general ivy-hydra general auctex all-the-icons-dired eshell-z esh-autosuggest org-bullets ob-ipython geiser lua-mode ccls company-lsp lsp-ui lsp-mode flycheck all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil magit eyebrowse company-coq company use-package))
+ '(safe-local-variable-values '((org-babel-use-quick-and-dirty-noweb-expansion . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

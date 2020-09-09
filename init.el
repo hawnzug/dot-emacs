7d84d578273cc87a259aad311467f869d5592154
hawnzug <hawnzug@gmail.com>
2020-09-08 19:43:08 -0500

    Clean up init.el

--- init.el
+++ init.el
@@ -5,49 +5,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#F5F5F9" "#D70000" "#005F00" "#AF8700" "#1F55A0" "#AF005F" "#007687" "#0F1019"])
- '(custom-safe-themes
-   '("f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" default))
- '(fci-rule-color "#4E4E4E")
- '(jdee-db-active-breakpoint-face-colors (cons "#D0D0E3" "#009B7C"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#D0D0E3" "#005F00"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#D0D0E3" "#4E4E4E"))
- '(objed-cursor-color "#D70000")
  '(package-selected-packages
-   '(vterm lua-mode hl-todo magit-delta eglot haskell-mode hide-mode-line wgrep which-key-posframe find-file-in-project olivetti ivy-bibtex terminal-here company-posframe docker-tramp dockerfile-mode csv-mode doom-themes doom-modeline yaml-mode sml-mode org-plus-contrib imenu-list tuareg web-mode emmet-mode keyfreq org-tree-slide academic-phrases org-make-toc exec-path-from-shell xterm-color ivy-posframe avy fcitx ibuffer-vc dired-open symbol-overlay htmlize evil-matchit alert proof-general ivy-hydra general auctex eshell-z esh-autosuggest org-bullets ob-ipython ccls all-the-icons-ivy counsel hydra which-key rainbow-delimiters evil-surround evil magit eyebrowse company-coq company use-package))
- '(pdf-view-midnight-colors (cons "#0F1019" "#F5F5F9"))
- '(rustic-ansi-faces
-   ["#F5F5F9" "#D70000" "#005F00" "#AF8700" "#1F55A0" "#AF005F" "#007687" "#0F1019"])
+   '(vterm lua-mode hl-todo magit-delta eglot haskell-mode wgrep which-key-posframe find-file-in-project olivetti ivy-bibtex terminal-here company-posframe docker-tramp dockerfile-mode csv-mode doom-themes doom-modeline yaml-mode sml-mode org-plus-contrib imenu-list tuareg keyfreq org-make-toc exec-path-from-shell ivy-posframe avy fcitx ibuffer-vc dired-open symbol-overlay htmlize evil-matchit alert proof-general ivy-hydra general auctex ccls all-the-icons-ivy counsel hydra which-key evil-surround evil magit company-coq company use-package))
  '(safe-local-variable-values
    '((auto-save-visited-interval . 0)
      (dired-omit-extensions ".vo" ".vok" ".vos" ".aux" ".glob")
-     (org-babel-use-quick-and-dirty-noweb-expansion . t)))
- '(vc-annotate-background "#F5F5F9")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#005F00")
-    (cons 40 "#3a6c00")
-    (cons 60 "#747900")
-    (cons 80 "#AF8700")
-    (cons 100 "#bc7900")
-    (cons 120 "#c96c00")
-    (cons 140 "#D75F00")
-    (cons 160 "#c93f1f")
-    (cons 180 "#bc1f3f")
-    (cons 200 "#AF005F")
-    (cons 220 "#bc003f")
-    (cons 240 "#c9001f")
-    (cons 260 "#D70000")
-    (cons 280 "#b41313")
-    (cons 300 "#922727")
-    (cons 320 "#703a3a")
-    (cons 340 "#4E4E4E")
-    (cons 360 "#4E4E4E")))
- '(vc-annotate-very-old-color nil))
+     (org-babel-use-quick-and-dirty-noweb-expansion . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit default))))
+ '(org-level-2 ((t (:inherit default))))
+ '(org-level-3 ((t (:inherit default))))
+ '(org-level-4 ((t (:inherit default))))
+ '(org-level-5 ((t (:inherit default))))
+ '(org-level-6 ((t (:inherit default))))
+ '(org-level-7 ((t (:inherit default))))
+ '(org-level-8 ((t (:inherit default)))))

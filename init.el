73131e20224d8eb1d7c7bda7be1d2e1b909f94aa
hawnzug <hawnzug@gmail.com>
2020-10-27 10:52:57 -0500

    Test

--- init.el
+++ init.el
@@ -6,8 +6,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter vterm lua-mode hl-todo magit-delta eglot haskell-mode wgrep which-key-posframe find-file-in-project olivetti ivy-bibtex terminal-here company-posframe docker-tramp dockerfile-mode csv-mode doom-themes doom-modeline yaml-mode sml-mode org-plus-contrib imenu-list tuareg keyfreq org-make-toc exec-path-from-shell ivy-posframe avy fcitx ibuffer-vc dired-open symbol-overlay htmlize evil-matchit alert proof-general ivy-hydra general auctex all-the-icons-ivy counsel hydra which-key evil-surround evil magit company-coq company use-package))
+   '(markdown-mode tree-sitter-langs tree-sitter vterm lua-mode hl-todo magit-delta eglot haskell-mode wgrep which-key-posframe find-file-in-project olivetti ivy-bibtex terminal-here company-posframe docker-tramp dockerfile-mode csv-mode yaml-mode sml-mode org-plus-contrib imenu-list tuareg keyfreq org-make-toc exec-path-from-shell ivy-posframe avy fcitx ibuffer-vc dired-open symbol-overlay htmlize evil-matchit alert proof-general ivy-hydra general auctex all-the-icons-ivy counsel hydra which-key evil-surround evil magit company-coq company use-package))
  '(safe-local-variable-values
    '((auto-save-visited-interval . 0)
      (dired-omit-extensions ".vo" ".vok" ".vos" ".aux" ".glob")
      (org-babel-use-quick-and-dirty-noweb-expansion . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

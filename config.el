a4610d73a3a166c5e5d05e8efac1ff41cce00a3b
hawnzug <hawnzug@gmail.com>
2024-06-27 08:34:32 -0500

    [perfect-margin] init

--- config.el
+++ config.el
@@ -193,8 +193,23 @@
   (setq olivetti-minimum-body-width 40))
 
 (use-package perfect-margin
-  :ensure t)
-
+  :ensure t
+  :custom
+  (perfect-margin-visible-width 100)
+  :config
+  ;; enable perfect-mode
+  (perfect-margin-mode t)
+  ;; auto-center minibuffer windows
+  (setq perfect-margin-ignore-filters nil)
+  ;; auto-center special windows
+  (setq perfect-margin-ignore-regexps nil)
+  ;; add additinal bding on margin area
+  (dolist (margin '("<left-margin> " "<right-margin> "))
+  (global-set-key (kbd (concat margin "<mouse-1>")) 'ignore)
+  (global-set-key (kbd (concat margin "<mouse-3>")) 'ignore)
+  (dolist (multiple '("" "double-" "triple-"))
+      (global-set-key (kbd (concat margin "<" multiple "wheel-up>")) 'mwheel-scroll)
+      (global-set-key (kbd (concat margin "<" multiple "wheel-down>")) 'mwheel-scroll))))
 (use-package which-key
   :ensure t
   :defer 1
